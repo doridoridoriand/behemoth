43c9f1551d2ffa1fcbb81c4eabbecd4d33919d6f
doridoridoriand <i.am.eager.for.peace@gmail.com>
2016-12-20 00:39:22 +0900

    CSSの縦書きが効くように変更。文字エンコードをUTF-8に変更

--- gen_text.rb
+++ gen_text.rb
@@ -7,10 +7,15 @@ end
 
 PDFKit.configure do |config|
   config.default_options = {
-    no_background: false,
-    page_size: 'A4',
-    orientation: 'Landscape',
-    disable_smart_shrinking: true
+    no_background:           false,
+    encoding:                'UTF-8',
+    page_size:               'A4',
+    orientation:             'Landscape',
+    margin_top:              '0in',
+    margin_right:            '0in',
+    margin_left:             '0in',
+    margin_bottom:           '0in',
+    disable_smart_shrinking: false
   }
 end
 kit = PDFKit.new(html)
